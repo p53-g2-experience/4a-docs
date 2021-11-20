a3bdb0a5c3c1bb18c8e6490323855be055f2b09d
zaterdarker <jamontanap@unal.edu.co>
2021-11-20 05:52:28 -0500

    "Prueba dentro del main"

--- CLASE_Pruebas_Clase_/C4a.AP.05. account_ms/src/main/java/com/misiontic/account_ms/exceptions/AccountNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.misiontic.account_ms.exceptions;
-
-public class AccountNotFoundException extends RuntimeException {
-    public AccountNotFoundException(String message) {
-        super(message);
-    }
-}
--- CLASE_Pruebas_Clase_/C4a.AP.06. account_ms/Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM openjdk:17-jdk-alpine
-ARG JAR_FILE=target/*.jar
-COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]
-CMD gunicorn --bind 0.0.0.0:$PORT wsgi
